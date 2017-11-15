f2dfbe2283e08935e83f4e7da6653f438deefe57
Konstantin Zhukov <kz@icolab.com>
2017-11-15 17:54:21 +0300

    ProjectManager fixes

--- Project.sol
+++ Project.sol
@@ -4,13 +4,11 @@ import "./Owned.sol";
 
 contract Project is Owned {
 
-    address public creator;
-    string public applicationHash;
+    address public author;
     string public name;
 
-    function Project(address _creator, string _applicationHash, string _name) public {
-        creator = _creator;
-        applicationHash = _applicationHash;
+    function Project(address _author, string _name) public {
+        author = _author;
         name = _name;
     }
 }
