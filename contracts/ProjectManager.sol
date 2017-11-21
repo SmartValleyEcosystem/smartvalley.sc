cd16de523d732ff86a1d0c6b1c91542053dbd5af
Konstantin Zhukov <kz@icolab.com>
2017-11-21 17:17:09 +0300

    ProjectManager mapping fix

--- contracts/ProjectManager.sol
+++ contracts/ProjectManager.sol
@@ -6,9 +6,9 @@ import "./Project.sol";
 contract ProjectManager is Owned {
 
     address[] public projects;
-    mapping(bytes32 => address) projectsMap;
+    mapping(uint256 => address) projectsMap;
 
-    function addProject(bytes32 _externalId, string _name) external {
+    function addProject(uint256 _externalId, string _name) external {
         Project project = new Project(msg.sender, _name);
         projects.push(project);
         projectsMap[_externalId] = project;
