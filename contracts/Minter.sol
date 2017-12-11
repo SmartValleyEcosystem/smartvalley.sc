e80e8455c4bf7fa42886a121fa197860af30f6e9
Zakharov Maksim <Max89701@gmail.com>
2017-12-11 18:26:42 +0300

    add setTokenAddress

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -1,28 +1,33 @@
 pragma solidity ^ 0.4.18;
 
 import "./Owned.sol";
+import "./SmartValleyToken.sol";
 
 contract Minter is Owned {
 
     uint256 public amountToGift = 1200;
 
-    address[] public receivers;
-    mapping(address => uint) public receiversBalanceMap;
     mapping(address => uint) public receiversDateMap;
 
-    function Minter() public payable {}
+    SmartValleyToken private token;
+
+    function Minter(address tokenAddress) public payable {
+        token = SmartValleyToken(tokenAddress);
+    }
 
     function giftTokens () public returns(uint256) {
         require(receiversDateMap[msg.sender] == 0 || now - receiversDateMap[msg.sender] >= 3 days);
-        if (receiversDateMap[msg.sender] == 0) {
-        receivers.push(msg.sender);
-        }
-        receiversBalanceMap[msg.sender] += amountToGift;
+        token.mintTokens(msg.sender, amountToGift);
         receiversDateMap[msg.sender] = now;
-        return receiversBalanceMap[msg.sender];
+        return token.balanceOf(msg.sender);
     }
 
     function () payable public {}
+    
+    function setTokenAddress (address tokenAddress) public onlyOwner {
+        require(token != tokenAddress);
+        token = SmartValleyToken(tokenAddress);
+    }
 
     function setAmountToGift (uint256 _amountToGift) public onlyOwner {
         require(_amountToGift > 0);
