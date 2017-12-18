56826a76bfcad9fba858704e09330f4611fbdf00
Konstantin Zhukov <kz@icolab.com>
2017-12-18 18:42:53 +0300

    fixed migrations

--- migrations/5_ProjectManager_migration.js
+++ migrations/5_ProjectManager_migration.js
@@ -1,12 +1,16 @@
 var ProjectManager = artifacts.require("./ProjectManager.sol");
 var SmartValleyToken = artifacts.require("./SmartValleyToken.sol");
+var Scoring = artifacts.require("./Scoring.sol");
 
 module.exports = function(deployer) {
   var tokenInstance;
   SmartValleyToken.deployed()
   .then(function(t){
     tokenInstance = t;
-    return deployer.deploy(ProjectManager, t.address, 120, 10);
+    return Scoring.deployed();
+  })
+  .then(function(s) {
+    return deployer.deploy(ProjectManager, tokenInstance.address, 120, 10, s.address);
   })
   .then(function(){
     return ProjectManager.deployed();
