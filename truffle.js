9f4e8991c6a6a312b92200797a310403c1a2f448
Maxim Guryev <mg@smartvalley.io>
2017-12-18 11:24:58 +0300

    add rinkeby config

--- truffle.js
+++ truffle.js
@@ -7,6 +7,11 @@ module.exports = {
       port: 9545,
       network_id: "*", // Match any network id
       gasPrice: 1000000000
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4
     }
   }
 };
