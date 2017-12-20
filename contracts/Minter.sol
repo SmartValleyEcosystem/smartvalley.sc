f3ffd70135be47995abf908c9c33ced6d944ba6e
Zakharov Maksim <Max89701@gmail.com>
2017-12-20 14:57:59 +0300

    add constant

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -7,6 +7,8 @@ contract Minter is Owned {
 
     uint256 public amountToGift = 1200;
 
+    uint public constant REQUIRED_DAYS_FOR_RECEIVE = 3;
+
     mapping(address => uint) public receiversDateMap;
 
     SmartValleyToken public token;
@@ -23,7 +25,7 @@ contract Minter is Owned {
 
     function canGetTokens(address _receiverAddress) view public returns(bool) {
         require(_receiverAddress != address(0));
-        return receiversDateMap[_receiverAddress] == 0 || now - receiversDateMap[_receiverAddress] >= 3 days;
+        return receiversDateMap[_receiverAddress] == 0 || now - receiversDateMap[_receiverAddress] >= REQUIRED_DAYS_FOR_RECEIVE * 1 days;
     }
 
     function setTokenAddress (address _tokenAddress) public onlyOwner {
