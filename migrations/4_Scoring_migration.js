5eca41da7e9dca486acf9adfdf84ab8ea223627b
Maxim Guryev <mg@smartvalley.io>
2017-12-21 13:58:56 +0300

    fix max score question id 10

--- migrations/4_Scoring_migration.js
+++ migrations/4_Scoring_migration.js
@@ -11,7 +11,7 @@ module.exports = function(deployer, network, accounts) {
     var questions =      [1,  2, 3,   4,  5,  6, 7,  8,  9, 10, 11, 12, 13];
     var expertiseAreas = [1,  1, 1,   1,  4,  4, 2,  2,  2,  3,  3,  3,  3];
     var minScores =      [0,  0, 0, -15,  0,  0, 0,  0,  0,  0,  0,  0,  0];
-    var maxScores =      [6, 10, 3,   0, 10, 15, 8, 10, 10,  5,  6,  5, 10];
+    var maxScores =      [6, 10, 3,   0, 10, 15, 8, 10, 10,  7,  6,  5, 10];
 
     return scoringInstance.addQuestions(expertiseAreas, questions, minScores, maxScores);
   });
